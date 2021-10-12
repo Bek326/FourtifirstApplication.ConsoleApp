12fbe1a1ef14e8ad90eb8284f308ac0391cdd5cb
thedarkknight <bekking326@gmail.com>
2021-10-12 16:27:09 +0600

    2

--- FourtifirstApplication.ConsoleApp/Program.cs
+++ FourtifirstApplication.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace FourtifirstApplication.ConsoleApp
 {
@@ -6,7 +8,26 @@ namespace FourtifirstApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var classes = new []
+            {
+                new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+                new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+                new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+         
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+ 
+        static string [] GetAllStudents( Classroom [] classes )
+        {
+            // ???
+            return null;
+        }
+      
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
         }
     }
 }
