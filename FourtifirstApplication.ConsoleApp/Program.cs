4805a76404c03c422a9f225e88b9f488b1385b8a
thedarkknight <bekking326@gmail.com>
2021-10-12 16:17:51 +0600

    1

---
+++ FourtifirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FourtifirstApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
